9fc714a468d53670bb368f83107dd7d5a45fdca5
LW Marketer <85985967+lwmarketer@users.noreply.github.com>
2021-07-01 16:22:46 +0800

    bushu

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Copyright © 2016 SW-THEMES. All rights reserved.
+ */
+
+namespace Lovevox\CatalogAttributes\Setup;
+
+use Magento\Catalog\Model\Category\AttributeRepository;
+use Magento\Eav\Model\Attribute;
+use Magento\Eav\Model\Entity\Attribute\Backend\Datetime;
+use Magento\Eav\Model\Entity\Attribute\Source\Boolean;
+use Magento\Framework\Module\Setup\Migration;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Catalog\Setup\CategorySetupFactory;
+use Magento\Eav\Setup\EavSetupFactory;
+
+/**
+ * @codeCoverageIgnore
+ */
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * Category setup factory
+     *
+     * @var CategorySetupFactory
+     */
+    private $categorySetupFactory;
+    private $eavSetupFactory;
+
+    private $objectManager;
+
+    /**
+     * Init
+     *
+     * @param CategorySetupFactory $categorySetupFactory
+     */
+    public function __construct(CategorySetupFactory $categorySetupFactory, EavSetupFactory $eavSetupFactory, \Magento\Framework\ObjectManagerInterface $objectManager)
+    {
+        $this->categorySetupFactory = $categorySetupFactory;
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        /* cataglog attributes */
+        $settingAttributes = [
+            'delivery_from_days' => [
+                'type' => 'int',
+                'label' => 'Delivery From Days',
+                'input' => 'select',
+                'required' => false,
+                'sort_order' => 10,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+            ],
+            'delivery_to_days' => [
+                'type' => 'int',
+                'label' => 'Delivery To Days',
+                'input' => 'select',
+                'required' => false,
+                'sort_order' => 20,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+            ]
+        ];
+
+        foreach ($settingAttributes as $item => $data) {
+            $eavSetup->addAttribute(\Magento\Catalog\Model\Category::ENTITY, $item, $data);
+        }
+    }
+}
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Lovevox_DeliverFromDays',
+    __DIR__
+);
